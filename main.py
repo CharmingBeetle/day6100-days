c4cab8de9e6e3196e64fe380c3f593acbe1f1bc3
CharmingBeetle <104301944+CharmingBeetle@users.noreply.github.com>
2024-04-14 11:04:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("THE SUPER DUPER SECURE SKYNET SYSTEM, BOIIII!")
+print()
+print("Here goes....!")
+print()
+username = input("Please type your username: ")
+password = input("Please type your password: ")
+print("Thank you, one moment...processing...")
+if username == "Nina" and password == "yoohoo23":
+  print("Hey Nina! I'm so glad to see your smiling face again!     Can I get a what, what!!")
+elif username == "Winston" and password == "yoho23":
+  print("Winston my man! Did you get to sniffing dog butts all     day or what?!")
+elif username == "Emma" and password == "doodoo23":
+  print("My god Emma, I think you've lost more weight! Are you     sure you're eating well, sweety?!")
+else:
+  print("Whoever you are, you do not belong here! Let me get the   door for you...")
